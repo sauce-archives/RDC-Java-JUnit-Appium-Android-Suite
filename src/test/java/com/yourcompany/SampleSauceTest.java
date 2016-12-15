761e6ff4b2bfddc8eeaaf88319b153da74f4a8ac
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2016-12-15 09:21:29 -0800

    Add framework

---
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -0,0 +1,57 @@
+package com.yourcompany;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testobject.appium.junit.TestObjectTestResultWatcher;
+import org.testobject.rest.api.appium.common.TestObject;
+import org.testobject.appium.junit.TestObjectAppiumSuite;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.URL;
+import java.util.List;
+
+
+@TestObject(testObjectApiKey = "INSERT_API_KEY_HERE", testObjectSuiteId = INSERT_SUITE_NUMBER_HERE)
+@RunWith(TestObjectAppiumSuite.class)
+public class SampleSauceTest {
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Rule
+    public TestObjectTestResultWatcher resultWatcher = new TestObjectTestResultWatcher();
+    
+    private AndroidDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+   
+    	
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("testobject_api_key", resultWatcher.getApiKey());
+        capabilities.setCapability("testobject_test_report_id", resultWatcher.getTestReportId());
+
+        driver = new AndroidDriver(new URL("http://appium.testobject.com/wd/hub"), capabilities);
+
+        resultWatcher.setAppiumDriver(driver);
+
+    }
+
+    @Test
+    public void addContactTest() {
+    	
+        WebElement button = driver.findElement(By.className("android.widget.Button"));
+        button.click();
+        List<WebElement> textFieldsList = driver.findElements(By.className("android.widget.EditText"));
+        textFieldsList.get(0).sendKeys("Some Name");
+        textFieldsList.get(2).sendKeys("Some@example.com");
+        button.click();
+    }
+
+}
